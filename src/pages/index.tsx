ea0de677432351309835199e201022ed0947eefc
boluo <xiangwuz@outlook.com>
2024-04-23 00:32:25 +0800

    同步

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,39 @@
 import Head from "next/head";
 import Link from "next/link";
-
+import { Input } from "@/components/ui/input";
+import { Button, Upload } from "antd";
 import { api } from "@/utils/api";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
-  const { data: memeList } = api.meme.getAll.useQuery();
+  const { data: memeList, refetch: getMemeList } = api.meme.getAll.useQuery();
 
+  const { mutate: addMeme } = api.meme.create.useMutation({
+    onError: (e) => {
+      console.log("e.message", e.message);
+    },
+    onSuccess: async () => {
+      console.log("创建成功");
+      await getMemeList();
+    },
+  });
   console.log("memeList", memeList);
 
   return (
     <>
+      <button
+        onClick={() => {
+          addMeme({ name: "ewfa", imageUrl: "bbbb", description: "cccc" });
+        }}
+      >
+        新增
+      </button>
+      <Button type="primary">按钮酒店经理咖啡店监考老师</Button>
+      <div className="w-max bg-red-100">
+        <Upload>
+          <div>上传图片</div>
+        </Upload>
+      </div>
       <div>{memeList?.map((v) => <div key={v?.id}>{v?.name}</div>)}</div>
     </>
   );
--- src/server/api/routers/meme.ts
+++ src/server/api/routers/meme.ts
@@ -6,4 +6,21 @@ export const memeRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.meme.findMany();
   }),
+  create: publicProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        imageUrl: z.string(),
+        description: z.string().optional(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.meme.create({
+        data: {
+          name: input.name,
+          imageUrl: input.imageUrl,
+          description: input.description ?? "",
+        },
+      });
+    }),
 });
